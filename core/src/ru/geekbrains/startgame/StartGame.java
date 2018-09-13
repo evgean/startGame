9b6ab44ca47fc575ba88539631f90fdf8d60f145
Jevgenijs Andrejevs <evgean@gmail.com>
2018-09-13 12:43:36 +0300

    Initial commit

---
+++ core/src/ru/geekbrains/startgame/StartGame.java
@@ -0,0 +1,35 @@
+package ru.geekbrains.startgame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StartGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion bg;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		bg = new TextureRegion(new Texture("bg.jpg"), 0, 0, 2048, 2048);
+	}
+
+	@Override
+	public void render () {
+		batch.begin();
+		batch.draw(bg, 0, 0);
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/ru/geekbrains/startgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.startgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.startgame.StartGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StartGame(), config);
+	}
+}
